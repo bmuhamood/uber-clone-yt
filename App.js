2f3a47f951128681e0454cd8238a64c5ebf0c7e9
bmuhamood <bbosa2009@gmail.com>
2021-09-02 23:54:37 +0400

    resolved RNCSafeAreaProvider" error on IOS

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // 1) setup redux
 export default function App() {
   return (
     <Provider store={store}>
+    <SafeAreaProvider>
       <HomeScreen/>
+    </SafeAreaProvider>
     </Provider>
   );
 }
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
-
+import { Icon } from 'react-native-elements';
 const data = [
     {
         id: "123",
@@ -26,11 +26,19 @@ const NavOptions = () => {
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
                 <TouchableOpacity style={tw`p-2 p-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                <View>
                     <Image
                         style={{ width: 120, height: 120, resizeMode: "contain" }}
                         source={{ uri: item.image }}
                     />
                     <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                    <Icon 
+                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                        name="arrowright"
+                        color="white"
+                        type="antdesign"
+                    />
+                </View>
                 </TouchableOpacity>
             )}
         />
